651d357d6d4afc6ba9eeef6b37c0ee9b814837f8
Jeet Mahaur <132244263+jeetmahaur7@users.noreply.github.com>
2023-05-12 12:38:17 +0530

    Zomato Search Card Filter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './components/Search';
+
+
+const App = () => {
+  return (
+    <>
+      <Search />
+    </>
+  )
+}
+
+export default App;
